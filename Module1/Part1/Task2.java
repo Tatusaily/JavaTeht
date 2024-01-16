c5907e93f8eac5aae6f10c74cf77684b10003330
Tatusaily <tatu.sailynoja@gmail.com>
2024-01-16 11:28:42 +0200

    refactor

---
+++ Module1/Part1/Task2.java
@@ -0,0 +1,13 @@
+// Task2 prints a pre-specified message, which is a star pyramid
+public class Task2 {
+    public static void main(String[] args){
+        System.out.println(
+                """
+                            *
+                           ***
+                          *****
+                         *******
+                         \
+                        """);
+    }
+}
