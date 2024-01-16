e74a1e16010b5a568af7fb709d52013fc3840b67
Tatusaily <tatu.sailynoja@gmail.com>
2024-01-16 10:40:21 +0200

    Task1 done

---
+++ Module1/src/Task1.java
@@ -0,0 +1,7 @@
+/* Install the environment, create a new project, and write a simple program that greets the user with a fixed message.
+Run the program and make sure that it works. Commit your changes to GitHub. */
+public class Task1 {
+    public static void main(String[] args){
+        System.out.println("Hello boy!");
+    }
+}
