512df29b8b3dcc1445da0e789e58b169527868b7
Tatusaily <tatu.sailynoja@gmail.com>
2024-01-16 10:43:01 +0200

    Task2 done

---
+++ Module1/src/Task2.java
@@ -0,0 +1,11 @@
+// Task2 prints a pre-specified message, which is a star pyramid
+public class Task2 {
+    public static void main(String[] args){
+        System.out.println(
+                "    *\n" +
+                "   ***\n" +
+                "  *****\n" +
+                " *******\n" +
+                " ");
+    }
+}
