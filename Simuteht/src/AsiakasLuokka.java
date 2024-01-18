bce056998630aab138b140efb3b6816ac3e2c074
Tatusaily <tatu.sailynoja@gmail.com>
2024-01-18 15:27:26 +0200

    asiakas?

---
+++ Simuteht/src/AsiakasLuokka.java
@@ -0,0 +1,19 @@
+public class AsiakasLuokka {
+    public static void main(String[] args) {
+        Asiakas asse = new Asiakas();
+        Asiakas pesse = new Asiakas();
+    }
+}
+
+class Asiakas {
+    private int id = 0;
+    private long aloitusaika;
+    private long lopetusaika;
+
+    public Asiakas(){//constructor
+
+    }
+    public time(){//timestop
+
+    }
+}
