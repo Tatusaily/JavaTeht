c65324db842cea58335a0042dd1a08484cad9493
Tatu <tatu.sailynoja@gmail.com>
2024-01-20 21:30:46 +0200

    duplicatearr init

---
+++ Module1.4/src/Task3_duplicateArr.java
@@ -0,0 +1,16 @@
+/*
+Write a program that takes an array of integers as input from the user and removes all the duplicate numbers from it. The program should work as follows:
+
+Ask the user for the size of the array.
+Prompt the user to enter the integers into the array.
+Remove all the duplicate numbers from the array using the following logic:
+Create a new array that stores only one occurrence of each number.
+Keep only the first occurrence of each number and discard any subsequent occurrences.
+Print the resulting array without the duplicate numbers.
+In this assignment, you can make the new array as big as the original array, even though it may not be completely filled.
+ */
+public class Task3_duplicateArr {
+    public static void main(String[] args) {
+
+    }
+}
