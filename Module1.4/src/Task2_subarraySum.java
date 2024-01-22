6d5ec87864105fd7aa0c9d024bd58557f5969204
Tatu <tatu.sailynoja@gmail.com>
2024-01-22 18:03:42 +0200

    subarray sum ok

--- Module1.4/src/Task2_subarraySum.java
+++ Module1.4/src/Task2_subarraySum.java
@@ -8,27 +8,40 @@ import java.util.Scanner;
 public class Task2_subarraySum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("How many numbers in the array?");
         int size = scanner.nextInt();
         int[] numarray = new int[size];
 
         // make numarray
         for (int i = 0; i < size; i++) {
-            String suffix = switch (i) {    // this isn't quite right but good enough
-                case 1 -> "st";
+            String suffix = switch (i) {    // this isn't quite right but good enough.
+                case 1 -> "st";             // is not grammatically correct after 20.
                 case 2 -> "nd";
                 case 3 -> "rd";
                 default -> "th";
             };
-            System.out.printf("Please enter the %1d%2s integer", i+1, suffix);
+            System.out.printf("Please enter the %1d%2s integer: ", i+1, suffix);
             int num = scanner.nextInt();
             numarray[i] = num;
         }
 
         // iterate numarray
-        for (int num : numarray){
-
+        int biggest = 0;
+        int i_left = 0;
+        int i_right = 0;
+        for (int i = 0; i < numarray.length; i++) {
+            int sum = 0;
+            for (int j = i; j < numarray.length; j++) {
+                sum += numarray[j];
+                if (sum > biggest){
+                    i_left = i;
+                    i_right = j;
+                    biggest = sum;
+                }
+            }
         }
+        System.out.printf("Biggest subarray was %d at (%d -> %d).", biggest, i_left, i_right);
 
     }
 }
