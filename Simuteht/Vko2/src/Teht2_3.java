cdad063dbde62f6678b059c5604a8ab35abebc59
Tatu <tatu.sailynoja@gmail.com>
2024-01-24 16:55:03 +0200

    kommentti

--- Simuteht/Vko2/src/Teht2_3.java
+++ Simuteht/Vko2/src/Teht2_3.java
@@ -4,13 +4,18 @@ import java.util.Random;
 public class Teht2_3 {  // Annetaan ikäjoukko ja luodaan satunnaisesti samaa jakaumaa noudattava joukko.
     static Random rand = new Random();
     public static void main(String[] args) {
+
+        // Lista teennäisistä ikälukemista
         int[] agelist = {18, 18, 19, 19, 20, 21, 21, 22, 22, 22, 23, 24, 25, 26, 26, 26, 30, 35, 42};
+
+        // luodaan uusi lista ja generoidaan satunnaisia ikiä
         int[] newlist = new int[1000];
         for (int i = 0; i < 1000; i++) {
             int rand_i = rand.nextInt(agelist.length);
             int randage = agelist[rand_i];
             newlist[i] = randage;
         }
+
         System.out.println(Arrays.toString(newlist));
     }
 }
