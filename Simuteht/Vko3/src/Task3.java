b3b10e8fade3cef281f45fd9252f04d6137ddf76
Tatusaily <tatu.sailynoja@gmail.com>
2024-01-25 14:01:16 +0200

    clocka

---
+++ Simuteht/Vko3/src/Task3.java
@@ -0,0 +1,20 @@
+public class Task3 {
+    public class Kello{
+        private static Kello instance = null;
+        private int time;
+        private Kello(){    // private constructor
+        }
+        static Kello getInstance(){ // singleton checker
+            if (instance == null){
+                instance = new Kello;
+            }
+            return instance;
+        }
+        public int getTime(){
+            return time;
+        }
+        public void setTime(int num){
+            time = num;
+        }
+    }
+}
