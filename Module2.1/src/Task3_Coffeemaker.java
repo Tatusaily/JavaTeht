77ed4878f9f9bd33461883f31863191d2ae1c4de
Tatusaily <tatu.sailynoja@gmail.com>
2024-01-25 13:39:41 +0200

    alkaa

---
+++ Module2.1/src/Task3_Coffeemaker.java
@@ -0,0 +1,12 @@
+/*
+A coffee maker has a selector for coffee type (normal/espresso) and a selector for coffee amount (10–80 ml).
+It also has an of/off switch that behaves in the same way as TV of/off switch.
+Coffee type and amount selectors behave like TV channel switch, i.e., they can only be changed when the device is on,
+and the device remembers the selections even when it is switched off.
+ */
+public class Task3_Coffeemaker {
+    public static class Coffeemaker{
+        private boolean coffeetype; // true=normal, false=espresso
+
+    }
+}
