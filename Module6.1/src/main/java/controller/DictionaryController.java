e6215784176150749aa2e412bf535d619bf5786f
Tatusaily <tatu.sailynoja@gmail.com>
2024-02-27 15:25:23 +0200

    dictionary ready and sanitize

---
+++ Module6.1/src/main/java/controller/DictionaryController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import view.DictionaryView;
+import model.Dictionary;
+
+public class DictionaryController {
+
+    private DictionaryView gui;
+    private Dictionary dict = new Dictionary();
+
+    public DictionaryController(DictionaryView gui) {
+        this.gui = gui;
+    }
+
+    public String search(String word) {
+        // search for word in dictionary
+        return dict.search(word);
+    }
+    public void add(String word, String meaning) {
+        // add word to dictionary
+        dict.add(word, meaning);
+    }
+
+    public static void main(String[] args) {
+        DictionaryView.launch(DictionaryView.class);
+    }
+}
+
