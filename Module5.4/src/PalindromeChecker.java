09b8dbe41ac19da34095685576f08f4ff620272b
Tatu <tatu.sailynoja@gmail.com>
2024-03-05 11:29:18 +0200

    TDD Palindrome Revised. Now passes.

--- Module5.4/src/PalindromeChecker.java
+++ Module5.4/src/PalindromeChecker.java
@@ -1,6 +1,12 @@
 public class PalindromeChecker {
     boolean isPalindrome(String str) {
+        // Step 1: Convert the string to lowercase
+        str = str.toLowerCase();
+        // Step 2: Remove punctuation and spaces. i.e. Allow only alphanumeric characters.
+        str = str.replaceAll("[^a-zA-Z0-9]", "");
+        // Step 3: Compare the string with its reverse
         String reversed = new StringBuilder(str).reverse().toString();
+
         return str.equals(reversed);
     }
 }
